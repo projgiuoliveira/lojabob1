7eb1b864b278ebe6798324716b644d8041921ba4
projgiuoliveira <167815439+projgiuoliveira@users.noreply.github.com>
2024-05-27 18:10:04 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const mysql= require("mysql2")
+
+const connection = mysql.createConnection({
+    host: 'sql10.freemysqlhosting.net',
+    user: 'sql10701770',
+    password: 'uh4Vi5Y7dZ', 
+    database: 'sql10701770'
+})
+
+connection.connect()
+
+//inserir dados - insert into nometabela 
+function inserir(lg, sn,mail) {
+    let sql = "insert into users set ?"
+    let dados = {login:lg,senha:sn,email:mail}
+
+    connection.query(sql,dados, function(error,results,fields){
+        if(error) throw error
+        console.log("Cadastro com sucesso!")
+    })
+}
+inserir("Giulia","12345","giulia4@gmail.com")
+
+connection.end()
---
+++ teste.js
@@ -0,0 +1,3 @@
+const app = require('./app2')
+
+app.inserir('sprite', 'coca1234','sprite@gmail.com')
